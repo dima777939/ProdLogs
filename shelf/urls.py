491cf16e52d9d56b31a5da13ea792be0ed13a089
Dmitriy Chubatov <dima.don84@gmail.com>
2022-07-14 17:37:54 +0700

    orders add in shelf via ajax

--- shelf/urls.py
+++ shelf/urls.py
@@ -5,7 +5,7 @@ app_name = "shelf"
 
 urlpatterns = [
     path("", views.ShelfDetailView.as_view(), name="shelf_detail"),
-    path("add/<int:order_id>/", views.ShelfAddView.as_view(), name="shelf_add"),
+    path("add/", views.ShelfAddView.as_view(), name="shelf_add"),
     path(
         "remove/<int:order_id>/", views.ShelfRemoveView.as_view(), name="shelf_remove"
     ),
